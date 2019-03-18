7a92aaa1aa2eb873f32a0af70acdd9e65a6e75ec
Hector Flores <hecto932@gmail.com>
2019-03-17 20:23:07 -0600

    Showing other objects list

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net_core.Models;
+
+namespace platzi_asp_net_core.Controllers
+{
+  public class StudentController : Controller
+  {
+    public IActionResult Index()
+    {
+      var modelToView = new Student
+      {
+        Name = "Pepe Perez",
+        UniqueId = Guid.NewGuid().ToString()
+      };
+
+      return View(modelToView);
+    }
+    public IActionResult MultiStudents()
+    {
+      var studentList = GenerarAlumnosAlAzar();
+
+      ViewBag.dynamicThing = "La Monja";
+      ViewBag.Date = DateTime.Now;
+
+      return View("MultiStudents", studentList);
+    }
+
+    private List<Student> GenerarAlumnosAlAzar()
+    {
+      string[] name1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+      string[] lastname = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+      string[] secondName = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+      var studentList = from n1 in name1
+                         from n2 in secondName
+                         from a1 in lastname
+                         select new Student { Name = $"{n1} {n2} {a1}" };
+
+      return studentList.OrderBy((student) => student.UniqueId).ToList();
+    }
+  }
+}
