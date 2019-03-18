999f88984f21a6f7159d010d9caa4d5a31934d30
Hector Flores <hecto932@gmail.com>
2019-03-17 18:53:44 -0600

    Partial views

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using platzi_asp_net_core.Models;
 
@@ -8,15 +9,38 @@ namespace platzi_asp_net_core.Controllers
   {
     public IActionResult Index()
     {
-      var subject = new Subject{
-        UniqueId = Guid.NewGuid().ToString(),
-        Name="Programming"
+      var modelToView = new Subject{
+        Name="Programación",
+        UniqueId = Guid.NewGuid().ToString()
+      };
+
+      return View(modelToView);
+    }
+    public IActionResult MultiSubjects()
+    {
+      var listSubjects = new List<Subject>(){
+        new Subject{
+          Name="Matemáticas",
+          UniqueId = Guid.NewGuid().ToString()
+        } ,
+        new Subject{
+          Name="Educación Física",
+          UniqueId = Guid.NewGuid().ToString()
+        },
+        new Subject{
+          Name="Castellano",
+          UniqueId = Guid.NewGuid().ToString()
+        },
+        new Subject{
+          Name="Ciencias Naturales",
+          UniqueId = Guid.NewGuid().ToString()
+        }
       };
 
       ViewBag.dynamicThing = "La Monja";
       ViewBag.Date = DateTime.Now;
 
-      return View(subject);
+      return View("MultiSubjects", listSubjects);
     }
   }
 }
